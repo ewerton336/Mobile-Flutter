a37c56bf7af63a4278d268069f72987d602122b2
Ewerton  Guimarães <ewertonpg@hotmail.com>
2021-09-11 08:25:13 -0300

    adição do jogo de número aleatório

---
+++ app_jogo_numero_aleatorio/android/app/src/main/kotlin/com/example/app_jogo_numero_aleatorio/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app_jogo_numero_aleatorio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ app_jogo_numero_aleatorio/lib/home.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  String InfoResultado = "Teste";
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _appBar(),
+      body: _body(),
+      backgroundColor: Colors.white,
+    );
+  }
+
+  _appBar() {
+    return AppBar(
+      title: Text("Jogo do número aleatório"),
+      centerTitle: true,
+      backgroundColor: Colors.greenAccent,
+    );
+  }
+
+  _body() {
+    return SingleChildScrollView(
+        padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              _imagemNumeroAleatorio(),
+              _textoNumeroAleatorio(),
+              _botaoGerarNumeroAleatorio(),
+              _resultado(),
+
+            ]));
+  }
+
+
+  _imagemNumeroAleatorio() {
+    return Text(
+      'Numero Aleatório',
+      textAlign: TextAlign.center,
+      style: TextStyle(color: Colors.red, fontSize: 35.0),
+    );
+  }
+
+  _textoNumeroAleatorio() {
+    return Text(
+      'Pense em um número de 0 a 10 e clique em descobrir.',
+      textAlign: TextAlign.center,
+      style: TextStyle(color: Colors.red, fontSize: 35.0),
+    );
+  }
+
+
+  _botaoGerarNumeroAleatorio() {
+    return Padding(
+      padding: EdgeInsets.only(top: 20.0, bottom: 35.0),
+      child: Container(
+        height: 50.0,
+        child: RaisedButton(
+          onPressed: _gerarNumeroAleatorio,
+          child: Text("Descobrir",
+              style: TextStyle(color: Colors.white, fontSize: 35.0)),
+          color: Colors.blueAccent,
+        ),
+      ),
+    );
+  }
+
+  _resultado() {
+    return Text(
+      InfoResultado,
+      textAlign: TextAlign.center,
+      style: TextStyle(color: Colors.red, fontSize: 35.0),
+    );
+  }
+
+
+  void _gerarNumeroAleatorio() {
+    setState(() {
+      int numero = new Random().nextInt(10);
+      InfoResultado = numero.toString();
+    });
+  }
+}
