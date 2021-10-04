047417e5bf95c12b8219e12d71f4538fe61a34c5
Ewerton  Guimarães <ewertonpg@hotmail.com>
2021-10-04 20:15:26 -0300

    conclusão do app de leitura

---
+++ app_leitura_frase/android/app/src/main/kotlin/com/example/app_leitura_frase/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app_leitura_frase
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- app_leitura_frase/lib/Home.dart
+++ app_leitura_frase/lib/Home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -6,52 +7,143 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  TextEditingController _controllerCampo = TextEditingController();
+  bool _switchDia = false;
+  bool _switchTamanho = false;
+  double tamanhoFonte = 15;
+  var corFundo = Colors.black38;
+  var corTexto = Colors.black38;
 
-  _salvar() {}
+  _salvar() async {
 
-  _recuperar() {}
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool("dia", _switchDia);
+    await prefs.setBool("tamanho", _switchTamanho);
+
+    print("Salvo! Dia: $_switchDia.toString(), Tamanho: $_switchTamanho.ToString()");
+  }
+
+  _recuperar() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    setState(() {
+      _switchDia = prefs.getBool("dia")!;
+      _switchTamanho = prefs.getBool("tamanho")!;
+
+    });
+
+    print("Método Recuperar Dia: $_switchDia.toString(), Tamanho: $_switchTamanho.ToString()");
+  }
+
+  _remover() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    await prefs.remove("nome");
+
+    print("Método Remover");
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Manipulação de dados"),
+        title: Text("Frases"),
       ),
       body: Container(
         padding: EdgeInsets.all(32),
         child: Column(
           children: <Widget>[
-            Text(
-              "Nada salvo!",
-              style: TextStyle(fontSize: 20),
-            ),
-            TextField(
-              keyboardType: TextInputType.text,
-              decoration: InputDecoration(labelText: "Digite algo"),
-              controller: _controllerCampo,
-            ),
+            Row(children: <Widget>[
+              Text('Dia'),
+              _botaoSwitchDia(),
+              Text('Pequeno'),
+              _botaoSwitchTamanho()
+            ]),
             Row(
               children: <Widget>[
                 RaisedButton(
                   color: Colors.blue,
                   textColor: Colors.white,
                   padding: EdgeInsets.all(15),
-                  child: Text("Salvar", style: TextStyle(fontSize: 20)),
+                  child: Text("Salvar", style: TextStyle(fontSize: 15)),
                   onPressed: _salvar,
                 ),
                 RaisedButton(
                   color: Colors.blue,
                   textColor: Colors.white,
                   padding: EdgeInsets.all(15),
-                  child: Text("Recuperar", style: TextStyle(fontSize: 20)),
+                  child: Text("Recuperar", style: TextStyle(fontSize: 15)),
                   onPressed: _recuperar,
-                )
+                ),
+                RaisedButton(
+                  color: Colors.blue,
+                  textColor: Colors.white,
+                  padding: EdgeInsets.all(15),
+                  child: Text("Remover", style: TextStyle(fontSize: 15)),
+                  onPressed: _remover,
+                ),
               ],
+            ),
+            Row(
+                children: <Widget>[
+                  Flexible(child:
+                  SingleChildScrollView(
+                  child:
+                  Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent arcu orci, auctor tempor massa in, pellentesque ultrices eros. Donec tempor maximus nunc nec suscipit. Nam metus magna, commodo quis pretium eu, hendrerit in velit. Ut placerat posuere augue vitae scelerisque. Mauris vel dolor libero. Proin mi tortor, laoreet in ultrices in, posuere vitae lorem",
+                  style: TextStyle(fontSize: tamanhoFonte,  backgroundColor: corFundo, color: corTexto),)
+                  )
+                  )]
             )
           ],
         ),
       ),
     );
   }
+
+
+
+
+  _botaoSwitchDia() {
+    return Switch(
+      value: _switchDia,
+      onChanged: (value) {
+        setState(() {
+          _switchDia = value;
+          if (value == true)
+            {
+              corFundo = Colors.black;
+              corTexto = Colors.white;
+            }
+          else
+            {
+              corFundo = Colors.white;
+              corTexto = Colors.black;
+            }
+        });
+      },
+    );
+  }
+
+  _botaoSwitchTamanho() {
+    return Switch(
+      value: _switchTamanho,
+      onChanged: (value) {
+        setState(() {
+          _switchTamanho = value;
+          if (value == true)
+            {
+              tamanhoFonte = 25.0;
+            }
+          else
+            {
+              tamanhoFonte = 15.0;
+            }
+        });
+      },
+    );
+  }
+
+
+
+
+
 }
