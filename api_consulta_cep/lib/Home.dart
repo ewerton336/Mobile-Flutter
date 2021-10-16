f45c07d12dd4e3bf58f7a3bb98d67e9b8add8fd6
Ewerton  Guimarães <ewertonpg@hotmail.com>
2021-10-16 10:55:26 -0300

    conclusão da API

---
+++ api_consulta_cep/lib/Home.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  TextEditingController cepController = TextEditingController();
+  String info_endereco = '';
+
+  _recuperarCep() async {
+    String url = 'https://viacep.com.br/ws/${cepController.text}/json/';
+
+    print(url);
+
+    http.Response response = await http.get(Uri.parse(url));
+    Map<String, dynamic> retorno = json.decode(response.body);
+    String logradouro = retorno["logradouro"];
+    String bairro = retorno["bairro"];
+    String localidade = retorno["localidade"];
+    String uf = retorno["uf"];
+
+    setState(() {
+      info_endereco =
+      'Logradouro: $logradouro \n Bairro: $bairro \n Cidade: $localidade \n Estado: $uf';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Consulta de CEP"),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  labelText: "Digite o CEP",
+                  labelStyle: TextStyle(color: Colors.green)),
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.green, fontSize: 25.0),
+              controller: cepController,
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
+              child: Container(
+                height: 50.0,
+                child: RaisedButton(
+                  onPressed: _recuperarCep,
+                  child: Text("Obter endereço",
+                      style: TextStyle(color: Colors.white, fontSize: 20.0)),
+                  color: Colors.blue,
+                ),
+              ),
+            ),
+            Text(info_endereco),
+          ],
+        ),
+      ),
+    );
+  }
+}
